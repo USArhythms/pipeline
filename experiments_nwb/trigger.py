de532b15e33722bd21722e91a566cd65b230f9af
DR-SDCCD <47396346+drinehart11@users.noreply.github.com>
2024-11-19 14:02:28 -0800

    passing additional arguments to the script

--- experiments_nwb/trigger.py
+++ experiments_nwb/trigger.py
@@ -1,8 +1,16 @@
 import paramiko
+import socket
 
-host='obis.dk.ucsd.edu'
+compute_host='obis.dk.ucsd.edu'
 user='afassihizakeri'
 
+# Path to the script on the remote server (default is home directory of user)
+remote_submission_script_full_path = '~/post_acquire.sh'
+
+#####################################
+#AUTOMATIC COLLECTION OF SUBMISSION HOST INFO
+acquisition_host_name = socket.gethostname()
+acquisition_host_ip_address = socket.gethostbyname(acquisition_host_name)
 #####################################
 
 # Create an SSH client
@@ -11,15 +19,17 @@ ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 # Connect to the remote server
-ssh.connect(hostname=host, username=user)
+ssh.connect(hostname=compute_host, username=user)
 
-# Execute the remote script
-#Note: post_acquire.sh is located in home directory of user defined at top of script
-stdin, stdout, stderr = ssh.exec_command('bash ~/post_acquire.sh')
+# Full remote script with arguments (quotes added in case acquisition host name has spaces)
+command = f"bash {remote_submission_script_full_path} --host '{acquisition_host_name}' --ip {acquisition_host_ip_address}"
+channel = ssh.get_transport().open_session()
+channel.exec_command(command)
 
-# Print the output of the script
-print(stdout.read().decode())
-print(stderr.read().decode())
+# Print the stdout, stderr of the script
+std_out = channel.recv(1024).decode('utf-8')
+std_err = channel.recv_stderr(1024).decode('utf-8')
+print(std_out, std_err, sep='\n')
 
 # Close the connection
 ssh.close()
--- post-acquisition/post_acquire.sh
+++ post-acquisition/post_acquire.sh
@@ -1,22 +1,112 @@
 #!/bin/sh
 
+# post_acquire.sh
+# Version 1.0
+
 #POST-ACQUISITION SCRIPT TO BE RUN ON SERVER (LINUX)
 
 ###########################################################
-#USER-DEFINED VARIABLES:
-
-#LOCATION OF ACQUIRED FOLDERS (MOUNTED ON SERVER)
+#USER-DEFINED, SYSTEM VARIABLES:
 
 
 #LOCATION OF META-DATA EXTRACTION FILES FOR RECORDINGS (AFTER POST-ACQUISITION PROCESSING)
 
+
+#LOG FILE LOCATION (FOLDER)
+LOG_LOCATION="$HOME/log/"
+
+#SYSTEM VARIABLES
+PYTHON_VENV="/usr/local/share/behavior/bin/python3"
+COMPUTE_HOST=$(hostname)
 ###########################################################
 
-#RUN POST-ACQUISITION PROCESSING
+#CAPTURE ARGUMENTS PASSED TO SCRIPT FROM ACQUISITION COMPUTER
+SRC_HOST=""
+SRC_IP=""
 
+#DEBUG
+#echo "All arguments received: $@"
+#echo "Number of arguments: $#"
 
+#PARSE ARGUMENTS
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --host)
+            SRC_HOST="$2"
+            shift 2
+            ;;
+        --ip)
+            SRC_IP="$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            exit 1
+            ;;
+    esac
+done
 
+if [[ -z "$SRC_HOST" || -z "$SRC_IP" ]]; then
+    echo "Error: Both --host and --ip arguments are required."
+    exit 1
+fi
 
+echo "Host: $SRC_HOST"
+echo "IP: $SRC_IP"
+echo "COMPUTE_HOST:\t$COMPUTE_HOST\n"
+echo "RUN AS USER: $USER"
+###########################################################
+#LOG OPTIONS & INIT
+DATESTR="%Y-%m-%d %H:%M:%S"
+mkdir -p $LOG_LOCATION
+
+TIMESTAMP=$(date +"$DATESTR")
+read -r -d '' LOGSTR <<EOF
+START post_acquire.sh\n
+------------------------------------\n
+SRC_HOST:\t$SRC_HOST\n
+SRC_IP:\t$SRC_IP\n
+COMPUTE_HOST:\t$COMPUTE_HOST\n
+SUBMIT_TIMESTAMP:\t$TIMESTAMP\n
+------------------------------------\n
+EOF
+
+echo -e $LOGSTR > "$LOG_LOCATION/log.txt"
+
+###########################################################
+#POST-ACQUISITION PROCESSING (BASED ON HOST)
+
+case "$SRC_HOST" in
+  'lil-whisker')
+    #TOP CAMERA PROCESS
+    echo "Processing for top camera on $SRC_HOST" >> "$LOG_LOCATION/log.txt"
+    PROCESS_SCRIPT_LOCATION="/data/behavior/run_top_view.py"
+    ;;
+
+  "B6QTE70" | "gyri" | "88QP74G" | "protocerebrum-dk")
+    #SIDE CAMERA PROCESS (FOR ALL $SRC_HOST MATCHING PATTERN ABOVE)
+    echo "Processing for side camera on $SRC_HOST" >> "$LOG_LOCATION/log.txt"
+    PROCESS_SCRIPT_LOCATION="/data/behavior/run_side_view.py"
+    ;;
+
+  *)
+    #UNKNOWN
+    echo "UNKNOWN HOST OR NO POST ACQUISITION PROCESSING REQUIRED" >> "$LOG_LOCATION/log.txt"
+    PROCESS_SCRIPT_LOCATION=""
+    ;;
+esac
+
+# If a process script was set, execute it
+if [ -n "$PROCESS_SCRIPT_LOCATION" ]; then
+    CMD="$PYTHON_VENV $PROCESS_SCRIPT_LOCATION --host $SRC_HOST --log $LOG_LOCATION/log.txt --user $USER"
+    echo "Executing process script: $PROCESS_SCRIPT_LOCATION" >> "$LOG_LOCATION/log.txt"
+    $CMD
+else
+    echo "No process script to execute" >> "$LOG_LOCATION/log.txt"
+fi
+
+echo "$(date +"$DATESTR") - POST-ACQUISITION PROCESSING COMPLETED" >> "$LOG_LOCATION/log.txt"
+
+###########################################################
 
 #RUN META-DATA EXTRACTION
-
